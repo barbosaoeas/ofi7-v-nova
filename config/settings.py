6008974eddf75cbf3dc3b134e0c1e191b3b624dc
Hilarindo Barbosa <barbosa.eas@gmail.com>
2026-04-22 16:16:15 -0300

    senha

--- config/settings.py
+++ config/settings.py
@@ -111,19 +111,10 @@ LOGOUT_REDIRECT_URL = 'funcionarios:login'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
-            'min_length': 8,
+            'min_length': 4,
         }
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
