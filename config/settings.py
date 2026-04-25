bc3ced96c7aad4c520e86aa93d0baa1503a3784b
Hilarindo Barbosa <barbosa.eas@gmail.com>
2026-04-25 15:01:10 -0300

    acrec immagem fun

--- config/settings.py
+++ config/settings.py
@@ -127,11 +127,11 @@ USE_I18N = True
 USE_TZ = True
 
 # STATIC
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # MEDIA
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # DEFAULT PRIMARY KEY
